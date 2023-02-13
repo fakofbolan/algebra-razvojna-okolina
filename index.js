98fb8046823e38646342f6086987dba0516686ee
trc <dario.trcovic@gmail.com>
2023-02-13 18:51:03 +0100

    datum-22

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const dateEl = document.querySelector(`#date`);
     const today = new Date();
-    console.log()
-    let year = today.getFullYear();
+/*     let year = today.getFullYear();
     let month = today.getMonth() + 1;
     let day = today.getDate();
     let h = today.getHours();
     let min = today.getMinutes();
     let sec = today.getSeconds();
-    dateEl.innerHTML = `${h}h ${min}m ${sec}s <div>Godina:${year}<br>Mjesec: ${month}<br>Day:${day}</div>`;
+    dateEl.innerHTML = `${h}h ${min}m ${sec}s <div>Godina:${year}<br>Mjesec: ${month}<br>Day:${day}</div>`; */
+    dateEl.innerHTML = `${today.toLocaleString()}`;
 
 console.log(`Hello world!`)
