d09d7c1bb1067c614cffda089c6d10b21a5731ab
trc <dario.trcovic@gmail.com>
2023-02-13 18:43:17 +0100

    datum

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const dateEl = document.querySelector(`#date`);
+    const today = new Date();
+    console.log()
+    let h = today.getHours();
+    let min = today.getMinutes();
+    let sec = today.getSeconds();
+    dateEl.innerHTML = `${h}h ${min}m ${sec}s ${today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate()}`;
+
 console.log(`Hello world!`)
